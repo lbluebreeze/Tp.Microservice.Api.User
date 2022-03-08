fcf852afef17104a7ec83854f5302505d4328028
Juan Eduardo Padilla Gonzalez <jepadilla@nexabpo.com>
2022-03-08 18:35:55 -0500

    Añadir Middleware

---
+++ src/Tp.Microservice.Api.User/Configuration/Startup/Cors/CorsMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Linq;
+
+namespace Tp.Microservice.Api.User.Configuration.Startup.Cors
+{
+    /// <summary>
+    /// Clase encargada de configurar los permisos de orígenes cruzados
+    /// </summary>
+    public static class CorsMiddleware
+    {
+        /// <summary>
+        /// Método encargado de configurar los sitios permitidos para el origen cruzado desde variables de entorno
+        /// </summary>
+        public static void UseCustomCors(this IApplicationBuilder app)
+        {
+            var cors = Environment.GetEnvironmentVariable("CORS_ORIGINS")?.Split(",") ?? Array.Empty<string>();
+
+            if (cors.Any())
+            {
+                app.UseCors(x => 
+                    x.WithOrigins(cors)
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyHeader()
+                        .WithMethods("GET", "POST", "PUT", "DELETE")
+                        .AllowCredentials()
+                );
+            }
+        }
+    }
+}
